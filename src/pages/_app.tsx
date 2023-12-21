ea94e30dfb965aac1f8b5bfc74c1643556219ea9
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-21 13:27:13 -0400

    added basic context state handler

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// import context from ./context/MyContext.ts and it is called AppContextProvider
+import {AppContextProvider} from '@/context/MyContext'
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppContextProvider>
+      <Component {...pageProps} />
+    </AppContextProvider>
+  );
 }
