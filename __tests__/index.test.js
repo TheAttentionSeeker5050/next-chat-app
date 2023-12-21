b80f956f4274c4663815109e3aff0d9886df3cdd
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-21 14:51:22 -0400

    mapped src file as "@/" on jest config

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,9 +2,9 @@ import Home from "../src/pages/index";
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen, waitFor, act } from "@testing-library/react";
 import { describe } from "node:test";
-import {AppContextProvider} from "../src/context/MyContext";
+import {AppContextProvider} from "@/context/MyContext";
 
-// if can start the app, then the test is passed
+// if can start the Main page, and get the main tag, it means that the app is working
 describe("can-render-home-page", () => {
     it("should render without throwing an error", async () => {
 
