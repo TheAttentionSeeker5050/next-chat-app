9317735a81bd96aafc9129a13ff942c5d497f7c2
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-23 16:08:56 -0400

    fixed html tag containerization alert on p

--- src/components/DarkThemeToggleSwitch.tsx
+++ src/components/DarkThemeToggleSwitch.tsx
@@ -4,9 +4,9 @@ import { Switch } from "@material-tailwind/react";
 
 export default function DarkThemeToggleSwitch() {
     return (
-        <p className='flex flex-row gap-3'>
+        <div className='flex flex-row gap-3'>
             <span > Dark Mode Enabled:</span>
             <Switch color="blue" defaultChecked crossOrigin={undefined} />
-        </p>
+        </div>
     );
 }
