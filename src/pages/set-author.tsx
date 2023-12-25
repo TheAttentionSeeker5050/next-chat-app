5930b6465585e2e1bff1c163a13ab09ec9607edb
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-25 15:57:18 -0400

    moved form handler to page before adding author validation

--- src/pages/set-author.tsx
+++ src/pages/set-author.tsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google';
-import { Dispatch, SetStateAction } from 'react'; // Add the import statement for Dispatch and SetStateAction
+import { Dispatch, FormEvent, SetStateAction } from 'react'; // Add the import statement for Dispatch and SetStateAction
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] });
 import {useAppContext} from '@/context/MyContext';
 import SetAuthorForm from '@/components/SetAuthorForm';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 
 
@@ -15,6 +16,27 @@ export default function SetAuthor() {
 
   // get the context, for the moment, nightMode is a boolean, author is a string, and socketid is an empty string by default
   const { author, socketId, nightMode, setAuthor } = useAppContext();
+
+  // use the navigate hook to redirect to set-author page once author is set
+  let router = useRouter();
+  
+  // do some onSubmit function to set the author, use form handler as event param
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const formData = new FormData(e.currentTarget);
+      const newAuthor = formData.get('author') as string;
+
+      if (!newAuthor.trim()) {
+          alert('Author name cannot be empty');
+          return;
+      }
+
+      setAuthor(newAuthor);
+
+      // use the navigate hook to redirect to set-author page
+      await router.push('/', undefined, { shallow: true });
+  }
 
   return (
     <>
@@ -26,7 +48,7 @@ export default function SetAuthor() {
         className={`${inter.className} flex flex-col justify-center gap-8 items-center mt-10`}
         >
         <h1 className='text-2xl'>Set Author Name</h1>
-        <SetAuthorForm author={author} setAuthor={setAuthor} /> 
+        <SetAuthorForm author={author} setAuthor={setAuthor} submitHandler={submitHandler}/> 
       </main>
     </>
   );
