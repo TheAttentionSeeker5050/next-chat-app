91d533c1db34da22e0fa9ddef48ab66a8f6aa64e
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-25 15:15:40 -0400

    created validation for uuid and author name

--- __tests__/input-output.test.ts
+++ __tests__/input-output.test.ts
@@ -5,14 +5,46 @@ import {describe, expect, test, it} from '@jest/globals';
 // import our functions to test and validate
 import { createUUID } from '@/utils/formatters/createUUID';
 import { validateUUID } from '@/utils/validators/validateUUID';
+import { validateAuthorName } from "@/utils/validators/validateAuthorName";
 
 
 
-// describe the test suite
+// describe the test suite -----------------------------
 // test - create a uuid and validate correct format
-describe('createUUID test', () => {
+describe('createUUID function test', () => {
     it('should create a uuid and validate correct format', () => {
         const uuid = createUUID();
         expect(validateUUID(uuid)).toBe(true);
+    });
+
+    it('should return invalid uuid', () => {
+        expect(validateUUID('invalid uuid')).toBe(false);
+    });
+});
+
+// test - validate author name
+describe('validateAuthorName function test', () => {
+    it('should return true for valid author name', () => {
+        expect(validateAuthorName('validAuthorName')).toBe(true);
+    });
+
+    it('should return true for valid author name with numbers', () => {
+        expect(validateAuthorName('validAuthorName123')).toBe(true);
+    });
+
+    it('should return false for empty author name', () => {
+        expect(validateAuthorName('')).toBe(false);
+    });
+
+    it('should return false for author name with more than 35 characters', () => {
+        expect(validateAuthorName('a'.repeat(36))).toBe(false);
+    });
+
+    it('should return false for author name with special characters', () => {
+        expect(validateAuthorName('invalid author name!')).toBe(false);
+    });
+
+    it('should return false for author name with spaces', () => {
+        expect(validateAuthorName('invalid author name')).toBe(false);
     });
 });
