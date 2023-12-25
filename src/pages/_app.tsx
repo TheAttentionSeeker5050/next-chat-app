c2f923337e4cdc8dfd1a5b49534cedab732d1794
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-25 14:11:49 -0400

    finished adding fontawesome, as well as favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,11 @@ import type { AppProps } from 'next/app'
 // import context from ./context/MyContext.ts and it is called AppContextProvider
 import {AppContextProvider} from '@/context/MyContext'
 
+// fontawesome
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
