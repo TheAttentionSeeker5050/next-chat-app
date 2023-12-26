eb603200ca84bc81cea19031153a6887769b21ff
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-26 14:06:12 -0400

    added crud operations for models and separated them into multiple files

--- src/utils/models/Message.model.ts
+++ src/utils/models/Message.model.ts
@@ -1,10 +1,12 @@
 // import required models
-import { UserModel } from '@/utils/models/user.model';
+import { UserModel } from '@/utils/models/User.model';
 
-// the message model and it's crud operations
+// the message model definition ---------------------------------
 export interface MessageModel {
     sender: UserModel,
     content: string, // if it is an image or file, it should be a url which points to the file
     timestamp: Date,
     contentType: string, // text, image for the moment
 }
+
+// crud operations for this are handled by the conversation model
