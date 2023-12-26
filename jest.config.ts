ca478569940b9224d9c9cf77959c5bba57c58b78
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-26 16:18:48 -0400

    memmory mongodb is now configured on jest setup.

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
   testEnvironment: 'jsdom',
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFiles: ['<rootDir>/jest.env.setup.ts'], // Add this line
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,17 @@
+import { afterAll, beforeAll } from '@jest/globals';
 import '@testing-library/jest-dom';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+
+let mongod: MongoMemoryServer;
+
+async function createMockMongoDbUri() {
+  mongod = await MongoMemoryServer.create();
+  const uri = mongod.getUri();
+  process.env.MONGODB_URI = uri;
+}
+
+beforeAll(async () => {
+  await createMockMongoDbUri();
+});
+
