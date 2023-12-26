e8739cb41fb8fbc65d28225d10e153864a0a63ba
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-26 14:43:25 -0400

    before setting mongo mock db

---
+++ __tests__/user-model.test.ts
@@ -0,0 +1,9 @@
+import "@testing-library/jest-dom";
+import {describe, expect, test, it} from '@jest/globals';
+
+// import our functions to test and validate
+// the model functions are imported from utils/models
+import { createUser, deleteUser, getUserByUsername } from "@/utils/models/User.model";
+
+
+// describe the test suite -----------------------------
