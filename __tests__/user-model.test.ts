a456f21f8b275dab79db015baebc1362e960745f
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-27 05:35:40 -0400

    finished testing user model, made few adjustments

--- __tests__/user-model.test.ts
+++ __tests__/user-model.test.ts
@@ -21,8 +21,13 @@ describe("User Model", () => {
     db = connection.db('nextChatDb');
   });
 
+  // close the database connection after all tests
+  afterAll(async () => {
+    await connection.close();
+  });
+
   // create a user and check that it exists
-  it("should create a user", async () => {
+  it("should create, retrieve and delete an user", async () => {
 
     const users = db.collection('users');
 
@@ -30,11 +35,32 @@ describe("User Model", () => {
       username: 'testUser',
     };
 
-    // Create a user
-    const newUser = await createUser(mockUser.username, connection, db);
+    try {
+      // Create a user
+      const newUser = await createUser(mockUser.username, connection, db);
+      
+      // Assert that the user is created successfully
+      expect(newUser.username).toBe('testUser');
+      
+      // assert get user by username
+      const foundUser = await getUserByUsername(mockUser.username, connection, db);
 
-    // Assert that the user is created successfully
-    expect(newUser.username).toBe('testUser');
-    // Add more assertions as needed
+      // assert that the user is found
+      expect(foundUser?.username).toBe(newUser.username);
+
+      // delete the user
+      await deleteUser(newUser._id.toString(), connection, db);
+
+      // assert that the user is deleted
+      const deletedUser = await users.findOne({ _id: newUser._id });
+
+      // assert that the user is null
+      expect(deletedUser).toBeNull();
+
+    } catch (error) {
+      // assert that the error is null, in case of error it will fail the test
+      expect(error).toBeNull();
+    }
+
   });
 });
