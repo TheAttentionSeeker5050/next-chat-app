16f41a7837173ea09a613af8a0e6cb37128eb14e
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-27 05:26:12 -0400

    test user first case ran well

--- __tests__/user-model.test.ts
+++ __tests__/user-model.test.ts
@@ -4,16 +4,34 @@ import {describe, expect, test, it, beforeAll, afterAll} from '@jest/globals';
 // import our functions to test and validate
 // the model functions are imported from utils/models
 import { createUser, deleteUser, getUserByUsername } from "@/utils/models/User.model";
+import { Db, MongoClient, MongoClientOptions } from "mongodb";
 
 // describe the test suite -----------------------------
 describe("User Model", () => {
+  let connection : MongoClient;
+  let db : Db
+
+  // connect to the database before all tests
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGODB_URI as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as MongoClientOptions);
+
+    db = connection.db('nextChatDb');
+  });
+
   // create a user and check that it exists
   it("should create a user", async () => {
 
-    console.log('Jest MONGODB_URI:', process.env.MONGODB_URI);
+    const users = db.collection('users');
+
+    const mockUser = {
+      username: 'testUser',
+    };
 
     // Create a user
-    const newUser = await createUser('testUser');
+    const newUser = await createUser(mockUser.username, connection, db);
 
     // Assert that the user is created successfully
     expect(newUser.username).toBe('testUser');
