c6ea3ee1892793f10e04f186cf6cdd98ab9ee3f3
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2023-12-27 09:06:00 -0400

    added mongo connection test before and after methods

--- __tests__/mongo-connection.test.ts
+++ __tests__/mongo-connection.test.ts
@@ -1,21 +1,47 @@
 import "@testing-library/jest-dom";
-import {describe, expect, test, it, beforeAll, afterAll} from '@jest/globals';
+import {describe, expect, test, it, beforeAll, afterAll, afterEach} from '@jest/globals';
 
 // import mongo driver
 import { connect } from "@/utils/mongoDbDriver";
 import { before } from "node:test";
+import { Db, MongoClient, MongoClientOptions } from "mongodb";
 
 // describe the test suite -----------------------------    
 describe("MongoDB Connection", () => {
+
+    let connection : MongoClient;
+    let db : Db
+
+    // connect to the database before all tests
+    beforeAll(async () => {
+        connection = await MongoClient.connect(process.env.MONGODB_URI as string, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        } as MongoClientOptions);
+
+        db = connection.db('nextChatDb');
+    });
+
+    // close the database connection after all tests
+    afterAll(async () => {
+        await connection.close();
+    });
+    
+    // afterEach(async () => {
+    //     // cleanup the user and conversation collections
+    //     await db.collection('users').deleteMany({});
+    //     await db.collection('conversations').deleteMany({});
+    // });
+    
     it("should connect to the database", async () => {
-        const { db, client } = await connect();
+        // const { db, client } = await connect();
 
         // ensure that the db and client are defined
         expect(db).toBeDefined();
-        expect(client).toBeDefined();
+        expect(connection).toBeDefined();
 
         // close the connection
-        await client.close();
+        await connection.close();
     
     });
 });
