46f38412a30c3349000f03204fa4346378f455b7
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-01-04 07:56:43 -0400

    added messages updated listener

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ import { MessageModelFirebase } from '@/utils/models/Message.model';
 import { GetServerSideProps } from 'next';
 
 // firebase imports
-import { Database, connectDatabaseEmulator, getDatabase, off, onChildAdded, onChildRemoved, onValue, ref } from "firebase/database";
+import { Database, connectDatabaseEmulator, getDatabase, off, onChildAdded, onChildChanged, onChildRemoved, onValue, ref } from "firebase/database";
 import firebase from '@/firebase';
 
 
@@ -97,11 +97,24 @@ export default function Home({ messages}: {messages: MessageModelFirebase[]}) {
 
           setMessageList((prevMessages) => prevMessages.filter((message) => message._id !== newMessage._id));
         });
+
+        const onChildEditedListener = onChildChanged(messagesListRef, (snapshot) => {
+          const newMessage = snapshot.val();
+
+          setMessageList((prevMessages) => prevMessages.map((message) => {
+            if (message._id === newMessage._id) {
+              return newMessage;
+            } else {
+              return message;
+            }
+          }));
+        });
         
         // Clean up the listener when the component unmounts
         return () => {
           off(messagesListRef, 'child_added', childAddedListener);
           off(messagesListRef, 'child_removed', childRemovedListener);
+          off(messagesListRef, 'child_changed', onChildEditedListener);
         };
 
     }
